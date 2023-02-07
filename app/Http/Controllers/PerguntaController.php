0d037cdf8791b466a0a27ae119a91a7c59b537f5
H3lc1ooS <helciosilva@protonmail.com>
2023-02-07 18:34:10 +0100

    Corrigi a falha ao salvar os posts

--- app/Http/Controllers/PerguntaController.php
+++ app/Http/Controllers/PerguntaController.php
@@ -7,100 +7,46 @@ use App\Models\Pergunta;
 
 class PerguntaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $perguntas = Pergunta::all();
         return view('home', ['perguntas' => $perguntas]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        /*Recursos por Desenvolver
+            -Salvar Imagem
+        */
 
-        $validate = $request->validate([
-            'pergunta'=> ['required'],
-            'image' => ['nullable'],
-        ]);
-
-        /*
         $perguntas = new Pergunta;
         $perguntas->pergunta = $request->pergunta;
-
-
-        if ($request->hasFile('image') && $request->file('image')->isValid()) {
-            $requestImage = $request->image;
-            $extension = $requestImage->extension();
-            $imageName = md5($requestImage->getClientOrignalName() . strtotime("now") . "." . $extension);
-
-            $request->image->move(public_path('img/posts'), $imageName);
-            $perguntas->imagem = $imageName;
-            $perguntas->save();
-        }*/
-
+        $perguntas->save();
         return redirect('/')->with('msg', 'Pergunta Enviada com Sucesso');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $perguntas = Pergunta::findOrFail($id);
         return view('post', ['pergunta' => $perguntas]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
